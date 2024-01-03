ce241e4272346fb0dbbb0046438219d40c9fd12c
Umut yeşildal <umutyesildal97@gmail.com>
2024-01-03 14:41:55 +0100

    shitfolio token holders

---
+++ src/Holders.js
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import './Holders.css';
+
+const Holders = () => {
+  const [ownersData, setOwnersData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [tokenAddress, setTokenAddress] = useState('');
+  const [isPressed, setIsPressed] = useState(false);
+  const solregex = /(^[1-9A-HJ-NP-Za-km-z]{32,44}$)/g 
+  const isValidAddress = tokenAddress.match(solregex)
+  console.log(isValidAddress)
+  // Function to fetch data and extract owners
+  const fetchDataAndExtractOwners = async () => {
+    setIsLoading(true);
+
+    const url = "https://jittery-charmian-fast-mainnet.helius-rpc.com/"; // Replace with your API URL
+    const headers = {
+      "Content-Type": "application/json"
+    };
+
+      // Define the data for fetching total token supply
+    const dataTokenSupply = {
+      "jsonrpc": "2.0",
+      "id": 1,
+      "method": "getTokenSupply",
+      "params": [tokenAddress] // Using tokenAddress from the component's state
+    };
+    const dataProgramAccounts = {
+      // Your data for the post request
+      // Example data structure
+      "jsonrpc": "2.0",
+      "id": 1,
+      "method": "getProgramAccounts",
+      "params": [
+        "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+        {
+          "encoding": "jsonParsed",
+          "filters": [
+            {
+              "dataSize": 165
+            },
+            {
+              "memcmp": {
+                "offset": 0,
+                "bytes": tokenAddress
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+      // Perform the POST request
+  const postRequest = async (data) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data)
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return await response.json();
+  };
+
+  try {
+    const totalSupplyResponse = await postRequest(dataTokenSupply);
+    const totalSupply = totalSupplyResponse.result.value.uiAmount;
+    console.log(totalSupply)
+    const programAccountsResponse = await postRequest(dataProgramAccounts);
+    console.log(programAccountsResponse)
+    const accounts = programAccountsResponse.result;
+    const processedData = accounts.reduce((acc, item) => {
+      const info = item.account.data.parsed.info;
+      const amount = info.tokenAmount.uiAmount;
+      if (amount > 0) {
+        const percentage = (amount / totalSupply) * 100;
+        acc.push({ owner: info.owner, amount, percentage: percentage.toFixed(2) });
+      }
+      return acc;
+    }, []);
+
+    processedData.sort((a, b) => parseFloat(b.percentage) - parseFloat(a.percentage));
+    console.log(processedData)
+    setOwnersData(processedData); // Update the state with the processed data
+  } catch (error) {
+    console.error('Error fetching and processing data: ', error);
+  } finally {
+    setIsLoading(false); // Ensure loading state is updated even if an error occurs
+  }
+    setIsPressed(true)
+    setIsLoading(false);
+  };
+
+  // Function to copy all owners' data to clipboard
+  const copyAllToClipboard = () => {
+    const allOwnersText = ownersData.join('\n'); // Join all owners with newline
+    navigator.clipboard.writeText(allOwnersText)
+      .then(() => alert('All owners copied to clipboard'))
+      .catch(err => console.error('Could not copy text: ', err));
+  };
+
+  const downloadOwnersData = () => {
+    const jsonBlob = new Blob([JSON.stringify(ownersData, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(jsonBlob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'ownersData.json';
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+
+  return (
+    <div className="holders">
+      <h1>Get all the token holders easily</h1>
+      <img src="/tokenholders.png" className="holders-logo" alt="tokenholders" />
+      <p>Explore the token universe with our feature that fetches and displays all holders of a specific token, <br></br> revealing each holder's percentage share. Gain quick and insightful access to ownership distribution in just a few clicks.</p>
+      {isValidAddress ? null : <span style={{color: "#EE4B2B"}}> Please enter a correct token address </span>}
+      <div className='input' >
+      <input style={isValidAddress ? {border: "2px solid #61dafb"} : {border: "2px solid red"} } 
+            type="text"
+            placeholder="Enter token address"
+            value={tokenAddress}
+            onChange={(e) => setTokenAddress(e.target.value)} 
+                     />
+      <button onClick={fetchDataAndExtractOwners} disabled={!isValidAddress || isLoading}>
+        {isLoading ? 'Loading...' : 'Fetch Data'}
+      </button>
+      </div>
+      { !isPressed ? null : <div className="owners-list">
+      {ownersData.length > 0 && (
+        <div className='owners-button' > 
+        <button onClick={downloadOwnersData}>Download Owners Data</button>
+        <button onClick={copyAllToClipboard}>Copy All Owners</button>
+        </div>
+      )}
+        <table className="owners-table">
+          <thead>
+            <tr>
+              <th>Owner</th>
+              <th>Amount</th>
+              <th>Percentage</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ownersData.map((owner, index) => (
+              <tr key={index}>
+                <td>{owner.owner}</td>
+                <td>{owner.amount}</td>
+                <td>{owner.percentage}%</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>}
+    </div>
+  );
+};
+
+export default Holders;
---
+++ src/Shitfolio.js
@@ -0,0 +1,193 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Shitfolio.css';
+import Wallet from './Wallet'; 
+import LoadingModal from './LoadingModal';
+
+function Shitfolio() {
+  const [portfolioData, setPortfolioData] = useState(null);
+  const [transactionData, setTransactionData] = useState(null);
+  const [walletAddress, setWalletAddress] = useState('');
+  const walletRef = useRef(null); // Creating a reference for the wallet section
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  // Function to fetch portfolio data from the API
+  const fetchPortfolioData = async () => {
+    if (!walletAddress.match(/^[1-9A-HJ-NP-Za-km-z]{32,44}$/)) {
+      alert('Invalid wallet address format.');
+      return;
+    }
+
+    const apiUrl = `https://public-api.birdeye.so/v1/wallet/token_list?wallet=${walletAddress}`;
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': '755089198a7140aa9f5bf0dd9ba84c18',
+        'x-chain': 'solana'
+      }
+    });
+
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      if (jsonResponse.success) {
+        const filteredItems = jsonResponse.data.items.filter(item => item.valueUsd != null);
+        jsonResponse.data.items = filteredItems;
+        console.log(jsonResponse.data)
+        setPortfolioData(jsonResponse.data);
+      } else {
+        console.error('API responded with success: false');
+      }
+    } else {
+      console.error('HTTP error: ', response.status);
+    }
+    
+  };
+
+    // Function to fetch transaction data from the API
+    const fetchTransactionData = async () => {
+      if (!walletAddress.match(/^[1-9A-HJ-NP-Za-km-z]{32,44}$/)) {
+        alert('Invalid wallet address format.');
+        return;
+      }
+      const apiUrl = `https://public-api.birdeye.so/v1/wallet/tx_list?wallet=${walletAddress}`;
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'GET',
+          headers: {
+            'X-API-KEY': '755089198a7140aa9f5bf0dd9ba84c18',
+            'x-chain': 'solana'
+          }
+        });
+  
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+  
+        const data = await response.json();
+        if (data.success) {
+          setTransactionData(calculateSOLPerformance(data.data.solana));
+        } else {
+          console.error('API responded with success: false');
+        }
+      } catch (error) {
+        console.error('Error fetching transaction data: ', error.message);
+      }
+    };
+
+
+    function calculateSOLPerformance(transactions) {
+      const solPerformance = {};
+    
+      transactions.forEach(tx => {
+        let solChange = 0;
+        let tokenData = {};
+    
+        tx.balanceChange.forEach(change => {
+          if (change.symbol === "SOL") {
+            solChange = change.amount;
+          } else {
+            tokenData = {
+              symbol: change.symbol,
+              logoURI: change.logoURI
+            };
+          }
+        });
+    
+        if (tokenData.symbol && !tokenData.symbol.startsWith("USD") && !tokenData.symbol.startsWith("WSOL")) {
+          const tokenName = tokenData.symbol;
+    
+          if (!solPerformance[tokenName]) {
+            solPerformance[tokenName] = { buys: 0, sells: 0, net: 0, logoURI: tokenData.logoURI };
+          }
+    
+          const convertedSolChange = Math.floor(solChange / Math.pow(10, 9 - 3)) / 1000;
+    
+          if (solChange < 0) {
+            solPerformance[tokenName].buys += Math.abs(convertedSolChange);
+          } else if (solChange > 0) {
+            solPerformance[tokenName].sells += convertedSolChange;
+          }
+    
+          solPerformance[tokenName].net += convertedSolChange;
+        }
+      });
+    
+      // Filter out tokens with only buys and no sells
+      Object.keys(solPerformance).forEach(token => {
+        if (solPerformance[token].sells === 0 && solPerformance[token].buys > 0) {
+          delete solPerformance[token];
+        }
+      });
+      
+      const sortedTokens = Object.entries(solPerformance)
+      .sort((a, b) => b[1].net - a[1].net);
+  
+    // Tag the best and worst plays (assuming there are at least two tokens)
+    if (sortedTokens.length > 1) {
+      const bestPlay = sortedTokens[0][0]; // The token with the highest net
+      const worstPlay = sortedTokens[sortedTokens.length - 1][0]; // The token with the lowest net
+      solPerformance[bestPlay].tag = 'Best Play';
+      solPerformance[worstPlay].tag = 'Worst Play';
+    }
+    
+      // Calculate total SOL change from the filtered solPerformance data
+      let totalSOLChange = Object.values(solPerformance).reduce((acc, token) => acc + token.net, 0);
+    
+      console.log({ performance: solPerformance, totalSOLChange });
+
+      return { performance: solPerformance, totalSOLChange };
+    }
+    
+
+  useEffect(() => {
+    if (portfolioData && walletRef.current) {
+      walletRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [portfolioData]);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    await fetchTransactionData();
+    await fetchPortfolioData();
+    setIsLoading(false);
+  };
+
+
+
+  return (
+    <div className="Shitfolio">
+      {isLoading && <LoadingModal />}
+      <header className="Shitfolio-header">
+        <h1>SHITFOLIO</h1>
+        <img src="/shitfolio.png" className="Shitfolio-logo" alt="logo" />
+        <p>Ever wondered how you did with your shitcoin gambles? <br></br></p>
+        <p style={{fontSize: "1vw"}} >The Statistics data is based on your last 100 transactions. <br></br> SOL Change is calculated with buys and sells, if you interacted with them differently it might be incorrect.</p>
+        <a style={{fontSize: "1vw", color: "white", paddingBottom: "1vw"}}  href='https:/twitter.com/yesilNFT'> A project by @YesilNFT </a>
+        <p style={{fontSize: "1vw", color:"yellow"}} >If you like and want to support me, send me any shitcoin you got GHp1YtXxwwPEcijzYod8RkZ1o3HgFaLtETWu6RUvKUmG</p>
+        
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Enter your wallet address"
+            value={walletAddress}
+            onChange={(e) => setWalletAddress(e.target.value)}
+          />
+          <button onClick={fetchData} disabled={!walletAddress}>Fetch Data</button>
+        </div>
+      </header>
+      
+        <main ref={walletRef}>
+          {portfolioData && transactionData ? (
+            <Wallet data={portfolioData} transactionData={transactionData} />
+          ) : ( null
+          )}
+        </main>
+
+    </div>
+    
+  );
+}
+
+export default Shitfolio;
